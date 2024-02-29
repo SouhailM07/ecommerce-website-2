3ecf9c715ee5fad1a6dadcadd0b118079d717d04
souhail <lightshadow416@gmail.com>
2024-02-29 22:20:57 +0100

    [Navbar] component checkpoint-lg

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,53 @@
+"use client";
 import "./navbar.css";
+// ? types
+import { navLinks } from "@/types";
+//
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+/*==============================================================================================*/
+// main component section
+/*==============================================================================================*/
+
 export default function Navbar() {
-  return <>Navbar</>;
+  const navLinks: navLinks[] = [
+    { txt: "Home", link: "" },
+    { txt: "All Products", link: "/products" },
+    { txt: "Categories", link: "/categories" },
+    { txt: "Account", link: "/account" },
+    { txt: "Cart", link: "/cart" },
+  ];
+  const handleSearch = () => {};
+  return (
+    <>
+      <header className="px-[2rem] py-[0.8rem] bg-slate-800 sticky top-0  max-w-[80rem] mx-auto">
+        <nav className="grid grid-cols-[20%_1fr_20%] items-">
+          <span className="text-white">Ecommerce</span>
+          <ul
+            role="list"
+            className="flex space-x-[2.5rem] text-[0.9rem] text-gray-400 place-self-center"
+          >
+            {navLinks.map((e, i) => {
+              return (
+                <li key={i} role="listitem" className="hover:text-white">
+                  <a href="#">{e.txt}</a>
+                </li>
+              );
+            })}
+          </ul>
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            aria-label="search btn"
+            title="search"
+            role="button"
+            height="25"
+            width="25"
+            className="text-gray-500 place-self-end aspect-square hover:text-white"
+            onClick={handleSearch}
+          />
+        </nav>
+      </header>
+    </>
+  );
 }
