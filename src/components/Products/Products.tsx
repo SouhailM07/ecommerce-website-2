da4ef20722e64f7aead5de45c2b301c6f948d7ff
souhail <lightshadow416@gmail.com>
2024-02-29 20:15:54 +0100

    components were created

---
+++ src/components/Products/Products.tsx
@@ -0,0 +1,4 @@
+import "./products.css";
+export default function Products() {
+  return <>Products</>;
+}
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as MyContainer } from "./MyContainer/MyContainer";
+export { default as Hero } from "./Hero/Hero";
+export { default as Main } from "./Main/Main";
+export { default as MyCard } from "./MyCard/MyCard";
+export { default as Products } from "./Products/Products";
